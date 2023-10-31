7575a5e4b16e0bd1832c3fc14a67a6f55305a3e2
MohamedGoush <0x1110111@gmail.com>
2023-10-31 21:27:42 +0530

    Converter Screen is Worked

--- LOTR Converter/ContentView.swift
+++ LOTR Converter/ContentView.swift
@@ -8,14 +8,113 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Properties
+    
+    @State private var silverCount: String = "";
+    @State private var goldCount: String = "";
+    
+    
+    
+    //MARK: BODY
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack{
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea(.all)
+            
+            VStack{
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                HStack{
+                    
+                    //MARK: Silver Piece
+                    
+                    VStack{
+                        
+                        HStack{
+                            
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 33)
+                            
+                            Text("Silver Piece")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                            
+                        } .padding(.bottom, -5)
+                        
+                        TextField("Amount", text: $silverCount)
+                            .padding(7)
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(7)
+                          
+                    }
+                    
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                      
+                    
+                    //MARK: Gold Piece
+                    
+                    VStack {
+                        
+                        HStack{
+                            
+                            Text("Gold Piece")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                            
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 33)
+                            
+                        }
+                        .padding(.bottom, -10)
+                       
+                        
+                        TextField("Amount", text: $goldCount)
+                            .padding(7)
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(7)
+                            .multilineTextAlignment(.trailing)
+                    }
+                    
+                }
+                .padding()
+                .background(.black.opacity(0.5))
+                .cornerRadius(50)
+                
+                Spacer()
+                
+                VStack{
+                    
+                    HStack{
+                        
+                        Spacer()
+                        
+                        Image(systemName: "info.circle.fill")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
+                }
+            }
+          
+            
+            
+            
+        }//:Body
+    
     }
 }
 
